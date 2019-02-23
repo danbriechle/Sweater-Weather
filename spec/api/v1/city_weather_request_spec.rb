173ec82e6e1d42aeb118c7f6be5d4df2706c652d
Daniel Briechle <danbriechle@hotmail.com>
2019-02-23 14:50:38 -0700

    weather poro in place

--- spec/api/v1/city_weather_request_spec.rb
+++ spec/api/v1/city_weather_request_spec.rb
@@ -15,5 +15,11 @@ describe "sweater weather api" do
     data = JSON.parse(response.body)
 
     expect(data.keys).to eq(["location", "current", "hourly", "daily"])
+    expect(data["current"].keys).to eq(["feels", "current_temp", "visibility", "time", "humdity", "uv", "icon"])
+    expect(data["hourly"].count).to eq(8)
+    expect(data["hourly"].first.keys).to eq(["time", "temp"])
+    expect(data["daily"].count).to eq(5)
+    expect(data["daily"].first.keys).to eq(["chance", "hi", "low", "summary", "icon"])
   end
+
 end
