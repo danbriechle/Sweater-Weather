9660f917582856bddfff87ab741d3c1913d15cda
Daniel Briechle <danbriechle@hotmail.com>
2019-02-23 18:41:30 -0700

    adds photo sample functionality

--- spec/services/flickr_service_spec.rb
+++ spec/services/flickr_service_spec.rb
@@ -1,9 +1,16 @@
 require "rails_helper"
 describe "flickr service" do
-  it "can get get a background image" do
+  it "can get get a be queried for a variety of back ground images" do
     city_state = 'denver,co'
     response = FlickrService.image_query(city_state)
 
     expect(response.keys).to eq([:photos, :stat])
+  end
+
+  it "can get get a sample photo" do
+    city_state = 'denver,co'
+    response = FlickrService.sample_photo(city_state)
+
+    expect(response.keys).to eq([:id, :owner, :secret, :server, :farm, :title, :ispublic, :isfriend, :isfamily])
   end
 end
