7b6a9f50ad96c7a8e5a84f492a6690b9c2eeb4cf
Daniel Briechle <danbriechle@hotmail.com>
2019-02-23 17:12:16 -0700

    forecast request complete

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -1,14 +1,13 @@
 class Forecast
   def initialize(location)
     @location = location
-    @call = DarkSkyService.forecast_query(@location)
     @current = self.current
     @hourly = self.hourly
     @daily = self.daily
   end
 
   def current
-    cw = @call[:currently]
+    cw = DarkSkyService.forecast_query(@location)[:currently]
     {feels: cw[:apparentTemperature],
       current_temp: cw[:temperature],
          visibility: cw[:visibility],
@@ -19,7 +18,7 @@ class Forecast
   end
 
   def hourly
-    hours = @call[:hourly][:data][0..7]
+    hours = DarkSkyService.forecast_query(@location)[:hourly][:data][0..7]
     hours.map do |hour|
       { time: Time.at(hour[:time]),
          temp: hour[:temperature]}
@@ -27,7 +26,7 @@ class Forecast
   end
 
   def daily
-    days = @call[:daily][:data][0..4]
+    days = DarkSkyService.forecast_query(@location)[:daily][:data][0..4]
     days.map do |day|
      {chance: day[:precipProbability],
             hi: day[:temperatureHigh],
