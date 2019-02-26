8c9286571686a05b34c44f9ce951ef723df3907f
Daniel Briechle <danbriechle@hotmail.com>
2019-02-25 17:59:39 -0700

    adds user creation sad path

--- spec/api/v1/new_user_request_spec.rb
+++ spec/api/v1/new_user_request_spec.rb
@@ -16,4 +16,19 @@ describe 'new user' do
   expect(data.keys).to eq(["api_key"])
   expect(user.api_key).to eq(data["api_key"])
   end
+
+  it 'cannot make a user with bad info' do
+  email = ""
+  password = "password"
+
+
+  post '/api/v1/users', params: {email: email,
+                           password: password,
+              password_confirmation: password}
+
+  data = JSON.parse(response.body)
+  expect(response.status).to eq(404)
+  expect(data.keys).to eq(["problem"])
+  expect(data["problem"]).to eq("you did it not good")
+  end
 end
