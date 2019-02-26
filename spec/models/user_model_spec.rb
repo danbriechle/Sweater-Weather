38d6cbb46e42c5dac5a625e4cf0f9a6455812bfb
Daniel Briechle <danbriechle@hotmail.com>
2019-02-25 19:44:35 -0700

    adds uniquness validation to user email

--- spec/models/user_model_spec.rb
+++ spec/models/user_model_spec.rb
@@ -4,6 +4,7 @@ describe User do
   describe 'validations' do
     it {should validate_presence_of(:email)}
     it {should validate_presence_of(:password)}
+    it {should validate_uniqueness_of(:email)}
   end
 
   describe 'class methods' do
