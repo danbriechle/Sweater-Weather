2a2fadc1efad78867fafe18d260bb50316060e27
Daniel Briechle <danbriechle@hotmail.com>
2019-02-25 22:30:30 -0700

    finishes favorite index sad path

--- spec/api/v1/user_sees_favorites_spec.rb
+++ spec/api/v1/user_sees_favorites_spec.rb
@@ -23,4 +23,23 @@ describe "as a registered user" do
     expect(data[0]["current_weather"].keys).to eq(["location", "current", "hourly", "daily"])
     end
   end
+
+  it "cannot see favorites with bad info" do
+    location_1 = "denver,co"
+    location_2 = "taos,nm"
+    location_3 = "austin, tx"
+    user_1 = User.create(email: "dave@dave.com", password: "daves_really_good_password")
+    user_2 = User.create(email: "donna@dave.com", password: "donnas_really_good_password")
+    fav_1 = user_1.favorites.create(location: location_1)
+    fav_2 = user_1.favorites.create(location: location_2)
+    fav_3 = user_2.favorites.create(location: location_3)
+
+    get "/api/v1/favorites", params: {api_key: "poop_emoji"}
+
+    data = JSON.parse(response.body)
+
+    expect(response.status).to eq(401)
+    expect(data.keys).to eq(["problem"])
+    expect(data["problem"]).to eq("you did it not good")
+  end
 end
